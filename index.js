14b1c9bb08b4eadc951fe20ceaa3c8c73c2dc6bb
emaadnahed <emaadnahed@gmail.com>
2022-10-23 21:38:29 +0530

    modified with hasBlackJack

--- index.js
+++ index.js
@@ -2,15 +2,21 @@
 // 1. Create two variables, firstCard and secondCard.
 // Set their values a to a random number between 2-11
 let firstCard = 6
-let secondCard = 90
+let secondCard = 15
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 let sum = firstCard + secondCard
+let hasBlackJack = false
 
 // Set the conditions to check the game status.
 if (sum < 21) {
     console.log("Do you want to draw a new card?")}
 else if (sum === 21) {
-    console.log("Wohoo! You've got Blackjack!")}
+    console.log("Wohoo! You've got Blackjack!")
+    hasBlackJack = true;}
 else {
     console.log("You're out of the game!")}
+
+
+
+console.log(hasBlackJack)
